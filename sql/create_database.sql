817395b9316fca3020cee140c60ebe76f770b655
malcodeman <malcodeman@users.noreply.github.com>
2018-09-16 12:47:06 +0200

    Adds create database sql lab

---
+++ sql/create_database.sql
@@ -0,0 +1,15 @@
+GO
+CREATE DATABASE Sales
+ON
+(NAME = Sales_dat,
+FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\saledat.mdf',
+SIZE = 10,
+MAXSIZE = 50,
+FILEGROWTH = 5)
+LOG ON
+(NAME = Sales_log,
+FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\salelog.ldf',
+SIZE = 5 MB,
+MAXSIZE = 25 MB,
+FILEGROWTH = 5 MB);
+GO
