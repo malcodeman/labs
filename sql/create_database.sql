c96a4f0ebc22692233efe724527fb2879d0b775a
malcodeman <malcodeman@users.noreply.github.com>
2018-09-25 18:08:56 +0200

    Adds insert procedure

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,4 +1,3 @@
-GO
 CREATE DATABASE Sales
 ON
 (NAME = Sales_dat,
@@ -25,3 +24,23 @@ INSERT INTO Clients (FirstName, LastName, City)
   VALUES ('First name', 'Last name', NULL)
 
 CREATE UNIQUE INDEX ix_uq_first_name ON Clients (FirstName)
+GO
+
+CREATE PROCEDURE Clients_Insert (@FirstName varchar(50),
+@LastName varchar(50),
+@City varchar(50))
+AS
+BEGIN
+  INSERT INTO Clients
+    VALUES (@FirstName, @LastName, @City)
+END
+GO
+
+EXEC Clients_Insert 'John',
+                    'Doe',
+                    'New York'
+GO
+EXEC Clients_Insert @FirstName = 'Jane',
+                    @City = 'Chicago',
+                    @LastName = 'Doe'
+GO
